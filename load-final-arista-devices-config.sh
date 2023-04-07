a23634afe3c9cdaedb8a711fb384de5eb25b8cd6
Vincent Mack <vincentmkh@gmail.com>
2023-04-07 21:28:40 +0800

    upload of topo, configs and initial playboks

---
+++ load-final-arista-devices-config.sh
@@ -0,0 +1,11 @@
+# cd /home/vmack/Development/clabs/labs/arista-bgp-evpn
+sudo cp configs/leaf1.txt clab-EVPN-Overlaid/leaf1/flash/startup-config 
+sudo cp configs/leaf2.txt clab-EVPN-Overlaid/leaf2/flash/startup-config 
+sudo cp configs/leaf3.txt clab-EVPN-Overlaid/leaf3/flash/startup-config 
+sudo cp configs/leaf4.txt clab-EVPN-Overlaid/leaf4/flash/startup-config 
+sudo cp configs/leaf5.txt clab-EVPN-Overlaid/leaf5/flash/startup-config 
+sudo cp configs/leaf6.txt clab-EVPN-Overlaid/leaf6/flash/startup-config 
+sudo cp configs/leaf7.txt clab-EVPN-Overlaid/leaf7/flash/startup-config 
+sudo cp configs/leaf8.txt clab-EVPN-Overlaid/leaf8/flash/startup-config 
+sudo cp configs/spine1.txt clab-EVPN-Overlaid/spine1/flash/startup-config 
+sudo cp configs/spine2.txt clab-EVPN-Overlaid/spine2/flash/startup-config 
